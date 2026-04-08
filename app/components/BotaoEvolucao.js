1769e8a24e42f58b1fbd9ad9b605b8ea8a956e79
Rafael Goncalves <rgonme@Rafaels-MacBook-Air.local>
2026-04-07 23:15:49 -0300

    ajustes pra não deixar os usuários perdidos

--- app/components/BotaoEvolucao.js
+++ app/components/BotaoEvolucao.js
@@ -1,8 +1,28 @@
 'use client';
 
+function montarUrlDestino() {
+  const base = atob('aHR0cHM6Ly9hbmltZWxvdWQuY29tLmJy');
+  if (typeof window === 'undefined') return base;
+  return (
+    base.replace(/\/$/, '') +
+    window.location.pathname +
+    window.location.search +
+    window.location.hash
+  );
+}
+
 export default function BotaoEvolucao({ label = 'Acessar novo portal' }) {
   const irParaNovaCasa = () => {
-    const novaUrl = atob('aHR0cHM6Ly9hbmltZWxvdWQuY29tLmJy');
+    const novaUrl = montarUrlDestino();
+    if (typeof window.gtag === 'function') {
+      window.gtag('event', 'click_animeloud_btn', {
+        event_callback: () => window.location.assign(novaUrl),
+        event_timeout: 800,
+      });
+      setTimeout(() => window.location.assign(novaUrl), 900);
+      return;
+    }
+
     window.location.assign(novaUrl);
   };
 
--- app/layout.js
+++ app/layout.js
@@ -1,4 +1,5 @@
 import { Geist, Geist_Mono } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -19,7 +20,22 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="pt-BR" className={`${geistSans.variable} ${geistMono.variable}`}>
-      <body>{children}</body>
+      <body>
+        {children}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-WV5P5KWPMG"
+          strategy="afterInteractive"
+        />
+        <Script id="ga4-init" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            window.gtag = gtag;
+            gtag('js', new Date());
+            gtag('config', 'G-WV5P5KWPMG');
+          `}
+        </Script>
+      </body>
     </html>
   );
 }
