660463e04d0161b6e0b53b387102d55239229e72
Rafael Goncalves <rgonme@Rafaels-MacBook-Air.local>
2026-06-07 22:34:19 -0300

    redirecionamento 301 para animeloud - teste

--- app/layout.js
+++ app/layout.js
@@ -1,16 +1,4 @@
-import { Geist, Geist_Mono } from "next/font/google";
-import Script from "next/script";
 import "./globals.css";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata = {
   title: "AnimeLand",
@@ -19,23 +7,8 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="pt-BR" className={`${geistSans.variable} ${geistMono.variable}`}>
-      <body>
-        {children}
-        <Script
-          src="https://www.googletagmanager.com/gtag/js?id=G-WV5P5KWPMG"
-          strategy="afterInteractive"
-        />
-        <Script id="ga4-init" strategy="afterInteractive">
-          {`
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            window.gtag = gtag;
-            gtag('js', new Date());
-            gtag('config', 'G-WV5P5KWPMG');
-          `}
-        </Script>
-      </body>
+    <html lang="pt-BR">
+      <body>{children}</body>
     </html>
   );
 }
--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  async redirects() {
+    return [
+      {
+        source: "/:path*",
+        destination:
+          "https://animeloud.com.br/:path*?utm_source=animeland&utm_medium=301_redirect&utm_campaign=portal_antigo",
+        statusCode: 301,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
